a1687e5798847191631150d17a979039b4491df3
Arjun Patel <55220892+arjunpatel01@users.noreply.github.com>
2022-10-31 00:52:35 -0500

    connected checkin to mongo

--- WorkingWOHeroku/software-design-project/app.py
+++ WorkingWOHeroku/software-design-project/app.py
@@ -9,6 +9,9 @@ from flask_cors import CORS
 from flask import request
 import os
 from pymongo import MongoClient
+import certifi
+
+ca = certifi.where()
 
 
 # app = Flask(__name__, static_folder='./build', static_url_path='/')
@@ -28,7 +31,7 @@ app = Flask(__name__)
 # jbG1kDkSwwyZVssJ
 
 
-client = MongoClient('mongodb+srv://gwills:jbG1kDkSwwyZVssJ@cluster0.kdtylku.mongodb.net/test?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://gwills:jbG1kDkSwwyZVssJ@cluster0.kdtylku.mongodb.net/test?retryWrites=true&w=majority', tlsCAFile=ca)
 userDB = client['userInfo']
 users = userDB['random']
 projectDB = client['projects']
@@ -71,9 +74,10 @@ def members():
 def checkIn_hardware(projectid, hwset, qty):
     qty = int(qty)
     pj = projects.find_one({'projectID': projectid})
-    currQty = pj["HWSet"][hwset-1]
+    currQty = pj['HWSet'][int(hwset)-1]
+
     print(currQty)
-    projects.update({'projectID': projectid},{'$set': {'HWSet.' + str(hwset-1) + '.content': qty}})
+    # projects.update_one({'projectID': projectid},{'$set': {hwset: currQty + qty})
     return{
         "projectid": projectid,
         "hwset": hwset,
